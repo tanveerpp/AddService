9c8a32b745639678b0750f8e8c6ad497560392a8
tanveerpp <73474576+tanveerpp@users.noreply.github.com>
2025-06-27 13:04:20 +0530

    Update AddressController.java

--- src/main/java/com/example/demo/AddressController.java
+++ src/main/java/com/example/demo/AddressController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AddressController {
@@ -13,5 +15,10 @@ public class AddressController {
 	public List<Address> show()
 	{
 		return this.repo.findAll();
+	}
+	@PostMapping("/address")
+	public Address addAddress(@RequestBody Address ad)
+	{
+		return this.repo.save(ad);
 	}
 }
